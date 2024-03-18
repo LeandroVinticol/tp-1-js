2ed1f083ce027df2f15938e7bab95014f035640a
Leandro Vinticol <leovinticol@gmail.com>
2024-03-18 16:22:00 -0300

    ejercicio 7

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,10 +1,10 @@
 //6. Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
-let primerNum = prompt("Ingrese el primer número");
-let segundoNum = prompt("Ingrese el segundo número");
+let primerNum = prompt('Ingrese el primer número');
+let segundoNum = prompt('Ingrese el segundo número');
 if (primerNum > segundoNum) {
-  document.write("El mayor es ", primerNum);
+  document.write('El mayor es ', primerNum);
 } else if (primerNum < segundoNum) {
-  document.write("El mayor es ", segundoNum);
+  document.write('El mayor es ', segundoNum);
 } else {
-  document.write("Los dos valores son iguales");
+  document.write('Los dos valores son iguales');
 }
