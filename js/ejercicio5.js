dc34138eacaa397b9028b0bcae47b3801a87e99d
Leandro Vinticol <leovinticol@gmail.com>
2024-03-18 15:38:59 -0300

    ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,4 @@
+//5. Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+let primerNum = prompt('Ingrese el primer número');
+let segundoNum = prompt('Ingrese el segundo número');
+document.write('El resultado de la suma de los dos numeros es: ', parseInt(primerNum, 10)  + parseInt (segundoNum, 10));
