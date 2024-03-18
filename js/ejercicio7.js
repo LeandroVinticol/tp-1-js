62f01dab2fc11cbb4432a69f8747bc752d6a15ea
Leandro Vinticol <leovinticol@gmail.com>
2024-03-18 16:28:27 -0300

    ejercicio 8

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,13 +1,13 @@
 //7. Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
-let primerNum = prompt("Ingrese el primer número");
-let segundoNum = prompt("Ingrese el segundo número");
-let tercerNum = prompt("Ingrese el tercer número");
+let primerNum = prompt('Ingrese el primer número');
+let segundoNum = prompt('Ingrese el segundo número');
+let tercerNum = prompt('Ingrese el tercer número');
 if (primerNum === segundoNum && primerNum === tercerNum) {
-  document.write("Los tres numeros son iguales");
+  document.write('Los tres numeros son iguales');
 } else if (primerNum > segundoNum && primerNum > tercerNum) {
-  document.write("El mayor es ", primerNum);
+  document.write('El mayor es ', primerNum);
 } else if (segundoNum > tercerNum) {
-  document.write("El mayor es ", segundoNum);
+  document.write('El mayor es ', segundoNum);
 } else {
-  document.write("El mayor es ", tercerNum);
+  document.write('El mayor es ', tercerNum);
 }
---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+//8. Escribe un programa que pida un número y diga si es divisible por 2
+let num = prompt('Ingrese un número');
+if(num % 2 === 0){
+    document.write('El numero es divisible por 2');
+}else {
+    document.write('El numero no es divisible por 2');
+}
